697f01adbda3b8c7757a9402ebfb732132d1d62e
kanancateaucambresis <kanancateaucambresis@gmail.com>
2026-07-03 15:53:12 +0900

    Fix Vercel adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   output: "server",
